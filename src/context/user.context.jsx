80a631b4a929645eacb9f3c301ac642c02108b04
wjstpgh <sehoJ3756@gmail.com>
2022-09-29 17:42:09 +0900

    사용자 context 리듀서, 리덕스로 상태관리 변경

--- src/context/user.context.jsx
+++ src/context/user.context.jsx
@@ -1,5 +1,6 @@
 import { createContext, useEffect, useReducer } from "react";
 import { onAuthStateChangedListener, createUserDocumentFromAuth } from "../utils/firebase/firebase.utils";
+import { createAction } from "../utils/reducer/reducer.utils";
 
 export const UserContext = createContext({
   currentUser: null,
@@ -32,7 +33,7 @@ export const UserProvider = ({ children }) => {
   const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
 
   const setCurrentUser = (user) => {
-    dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+    dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
   }
 
   const value = { currentUser, setCurrentUser };
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import reportWebVitals from './reportWebVitals';
 import App from './App';
 import './index.scss';
 import { BrowserRouter } from 'react-router-dom';
-import { UserProvider } from './context/user.context';
 import { CartProvider } from './context/cart.context';
 import { CategoriesProvider } from './context/categories.constext';
 import { Provider } from 'react-redux';
@@ -15,13 +14,11 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <UserProvider>
-          <CategoriesProvider>
-            <CartProvider>
-              <App />
-            </CartProvider>
-          </CategoriesProvider>
-        </UserProvider>
+        <CategoriesProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </CategoriesProvider>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
