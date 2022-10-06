14dfc4430b5d9b9132228f5d9f73f13615c75055
wjstpgh <sehoJ3756@gmail.com>
2022-10-06 16:38:37 +0900

    서버리스 함수 와 결제 기능 완성

--- src/component/payment-form/payment-form.component.jsx
+++ src/component/payment-form/payment-form.component.jsx
@@ -1,26 +1,69 @@
-import { CardElement,useStripe,useElements } from "@stripe/react-stripe-js";
-import Button, { BUTTON_TYPE_CLASSES } from "../button/botton.component";
-import { PaymentFormContainer, FormContainer } from "./payment-form.styles";
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { selectCartTotal } from "../../store/cart/cart.selector";
+import { selectCurrentUser } from "../../store/user/user.selector";
+import { BUTTON_TYPE_CLASSES } from "../button/botton.component";
+import { PaymentFormContainer, FormContainer, PaymentButton } from "./payment-form.styles";
 
 const PaymentForm = () => {
-  const stripe=useStripe();
-  const elements=useElements();
+  const stripe = useStripe();
+  const elements = useElements();
+  const amount = useSelector(selectCartTotal);
+  const currentUser = useSelector(selectCurrentUser);
+  const [isProcessingPayment, setIsProcessingPayment] = useState(false);
 
-  const paymentHandler=async(e)=>{
+  const paymentHandler = async (e) => {
     e.preventDefault();
 
-    if(!stripe || !elements){
+    if (!stripe || !elements) {
       return;
     }
 
-    
-  }
+    setIsProcessingPayment(true);
+
+    const response = await fetch('/.netlify/functions/create-payment-intent', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ amount: amount * 100 }),
+    }).then((res) => res.json());
+
+    const { paymentIntent: { client_secret } } = response;
+
+    const paymentResult = await stripe.confirmCardPayment(client_secret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: {
+          name: currentUser ? currentUser.displayName : 'Guest'
+        }
+      }
+    });
+
+    setIsProcessingPayment(false);
+
+    if (paymentResult.error) {
+      alert(`결제 에러, ${paymentResult.error}`);
+    }
+    else {
+      if (paymentResult.paymentIntent.status === 'succeeded') {
+        alert('결제 성공');
+      }
+    }
+  };
+
   return (
     <PaymentFormContainer>
-      <FormContainer>
-        <h2>결제 카드 정보 입력 : </h2>
+      <FormContainer onSubmit={paymentHandler}>
+        <h2>카드 입력</h2>
         <CardElement />
-        <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>결제하기</Button>
+        <PaymentButton
+          buttonType={BUTTON_TYPE_CLASSES.inverted}
+          isLoading={isProcessingPayment}
+        >
+          결제하기
+        </PaymentButton>
       </FormContainer>
     </PaymentFormContainer>
   );
