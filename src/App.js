8966cbf24cffc29f41bf36d86e6929471b1d32d7
wjstpgh <sehoJ3756@gmail.com>
2022-10-13 17:18:04 +0900

    라우팅 페이지 별로 spliting, 모바일 화면 지원을 위한 스타일 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import Home from "./routes/home/home.component";
+import { GlobalStyle } from "./global.styles";
 import { Route, Routes } from "react-router-dom";
-import Navigation from "./routes/navigation/navigation.component";
-import Authentication from "./routes/authentication/authentication.component";
-import Shop from "./routes/shop/shop.component";
-import CheckOut from "./routes/checkout/checkout.component";
-import { useEffect } from "react";
+import { useEffect, lazy, Suspense } from "react";
 import { checkUserSession } from "./store/user/user.action";
 import { useDispatch } from "react-redux";
+import Spinner from "./component/spinner/spinner.component";
+
+const Home = lazy(() => import('./routes/home/home.component'));
+const Authentication = lazy(() => import('./routes/authentication/authentication.component'));
+const Navigation = lazy(() => import('./routes/navigation/navigation.component'));
+const Shop = lazy(() => import('./routes/shop/shop.component'));
+const CheckOut = lazy(() => import('./routes/checkout/checkout.component'));
 
 const App = () => {
   const dispatch = useDispatch();
@@ -16,14 +19,17 @@ const App = () => {
   }, []);
 
   return (
-    <Routes>
-      <Route path="/" element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path="shop/*" element={<Shop />} />
-        <Route path="auth" element={<Authentication />} />
-        <Route path="checkout" element={<CheckOut />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<Spinner />}>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path="shop/*" element={<Shop />} />
+          <Route path="auth" element={<Authentication />} />
+          <Route path="checkout" element={<CheckOut />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
 
